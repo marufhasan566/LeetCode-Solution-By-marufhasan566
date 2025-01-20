a89020e35b0cf17b32d18901b64b4ae1dd67b160
marufhasan566 <marufhasan.mh1@gmail.com>
2025-01-20 10:29:37 +0600

    Solution of 3427

---
+++ 3427._Sum_of_Variable_Length_Subarrays.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/sum-of-variable-length-subarrays/description/
+
+class Solution
+{
+public:
+    int subarraySum(vector<int> &nums)
+    {
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int start = max(0, (i - nums[i]));
+            for (int j = start; j <= i; j++)
+            {
+                sum += nums[j];
+            }
+        }
+        return sum;
+    }
+};
