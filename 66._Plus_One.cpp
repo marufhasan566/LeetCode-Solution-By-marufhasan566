f8123c787f324d9f0b1c5f13a0d623dc7d4304b0
marufhasan566 <marufhasan.mh1@gmail.com>
2025-01-31 14:53:32 +0600

    Updated & Accepted Solution of 66

--- 66._Plus_One.cpp
+++ 66._Plus_One.cpp
@@ -3,29 +3,67 @@
 class Solution
 {
 public:
-    vector<int> plusOne(vector<int> &digits)
+    vector<int> plusOne(vector<int> &v)
     {
-        string s;
-        for (int i = 0; i < digits.size(); i++)
+        // int sz= digits.size()-1;
+        // cout<<digits[sz];
+        // if(digits[sz]==9){
+        //     digits[sz]=1;
+        //     digits.push_back(0);
+        // }
+        // else{
+        //     digits[sz] = digits[sz]+1;
+        // }
+        // return digits;
+        stack<int> st;
+        for (auto i : v)
         {
-            s = s + to_string(digits[i]);
+            st.push(i);
         }
-        int value = 0;
-        for (char c : s)
+
+        int countZero = 0;
+        while (!st.empty())
         {
-            if (c >= '0' && c <= '9')
-                value = value * 10 + (c - '0');
-            cout << c << " ";
+            if (st.top() == 9)
+            {
+                countZero += 1;
+                st.pop();
+            }
+            else
+            {
+                break;
+            }
         }
-        value = value + 1;
-        cout << endl;
-        string s2 = to_string(value);
-        vector<int> v;
-        for (int i = 0; i < s2.size(); i++)
+        // cout << countZero << endl;
+        if (st.empty())
         {
-            cout << s2[i] << " ";
-            v.push_back(s2[i] - 48);
+            st.push(1);
+            while (countZero--)
+            {
+                st.push(0);
+            }
         }
+        else
+        {
+            int v = st.top() + 1;
+            st.pop();
+            st.push(v);
+            while (countZero--)
+            {
+                st.push(0);
+            }
+        }
+        // while (!st.empty()) {
+        //     cout << st.top() << " ";
+        //     st.pop();
+        // }
+        v.clear();
+        while (!st.empty())
+        {
+            v.push_back(st.top());
+            st.pop();
+        }
+        reverse(v.begin(), v.end());
         return v;
     }
 };
