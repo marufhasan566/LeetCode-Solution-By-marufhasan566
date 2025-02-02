fb1ec8c1acd302410623c224d77d7f6b960e9cd4
marufhasan566 <marufhasan.mh1@gmail.com>
2025-02-02 10:07:29 +0600

    Biweekly Contest 149, Q1 Solved

---
+++ 3438._Find_Valid_Pair_of_Adjacent_Digits_in_String.cpp
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/find-valid-pair-of-adjacent-digits-in-string/description/
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    string findValidPair(string s)
+    {
+        int arr[10] = {0};
+        for (int i = 0; i < s.length(); i++)
+        {
+            int x = s[i] - '0';
+            arr[x]++;
+        }
+        string res;
+        for (int i = 0; i < s.length() - 1; i++)
+        {
+            int x = s[i] - '0';
+            int x2 = s[i + 1] - '0';
+            if (x == arr[x] && x2 == arr[x2] && x != x2)
+            {
+                // cout << x << "\t" << x2 << endl;
+                // cout << to_string(x) << "\t" << to_string(x2) << endl;
+                res += to_string(x);
+                res += to_string(x2);
+                break;
+            }
+        }
+        // cout << res;
+        return res;
+    }
+};
