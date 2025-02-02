deaad2e04272fe434c9cd39ce5cf2ecea0473f58
marufhasan566 <marufhasan.mh1@gmail.com>
2025-02-02 10:05:30 +0600

    Weekly Contest 435, Q1 Solved

---
+++ Weekly Contest 435/Q1._Maximum_Difference_Between_Even_and_Odd_Frequency_I.cpp
@@ -0,0 +1,39 @@
+// https://leetcode.com/contest/weekly-contest-435/problems/maximum-difference-between-even-and-odd-frequency-i/?slug=maximum-manhattan-distance-after-k-changes&tab=description&region=global_v2
+
+class Solution
+{
+public:
+    int maxDifference(string s)
+    {
+        int ar[101] = {0};
+        for (int i = 0; i < s.length(); i++)
+        {
+            ar[s[i] - 'a']++;
+        }
+        // check
+        int diff = INT_MIN;
+        vector<int> even;
+        vector<int> odd;
+        for (int i = 0; i < 101; i++)
+        {
+            if (ar[i] != 0)
+            {
+                if (ar[i] % 2 == 0)
+                    even.push_back(ar[i]);
+                if (ar[i] % 2 == 1)
+                    odd.push_back(ar[i]);
+            }
+        }
+        for (int i = 0; i < even.size(); i++)
+        {
+            // cout << even[i] << " ";
+            for (int j = 0; j < odd.size(); j++)
+            {
+                // cout << odd[j] << " ";
+                diff = max(diff, (odd[j] - even[i]));
+            }
+        }
+        cout << diff << endl;
+        return diff;
+    }
+};
