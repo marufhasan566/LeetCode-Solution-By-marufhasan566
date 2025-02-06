1d21265f9005918d2873801dc0626eba4ef521a9
marufhasan566 <marufhasan.mh1@gmail.com>
2025-02-06 14:27:44 +0600

    Solution of 1726. Must revise the concept and the solution. Check the attached youtube link

---
+++ 1726._Tuple_with_Same_Product.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/tuple-with-same-product/description/
+// Help from YT: https://www.youtube.com/watch?v=TBCOBD-24oE&ab_channel=Techdose
+// Must Revise this problem along with the concept of the solution
+
+class Solution
+{
+    int nC2(int n) { return (n * (n - 1)) / 2; }
+
+public:
+    int tupleSameProduct(vector<int> &nums)
+    {
+        unordered_map<int, int> freq;
+        int n = nums.size();
+        for (int i = 0; i < n - 1; i++)
+            for (int j = i + 1; j < n; j++)
+                freq[nums[i] * nums[j]]++;
+        int count = 0;
+        for (auto [product, frequency] : freq)
+            count += 8 * nC2(frequency);
+        return count;
+    }
+};
