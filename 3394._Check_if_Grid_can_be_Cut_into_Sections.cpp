0b7bfe349bc595c4323ee5d66db7beacdbfd1b2b
marufhasan566 <marufhasan.mh1@gmail.com>
2025-03-25 23:36:51 +0600

    Added 3394. Just Added. DUA

---
+++ 3394._Check_if_Grid_can_be_Cut_into_Sections.cpp
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/check-if-grid-can-be-cut-into-sections/description/?envType=daily-question&envId=2025-03-25
+// https://www.youtube.com/watch?v=9-901PJWE8k&ab_channel=Techdose
+// Just Added The Solution
+
+class Solution {
+    #define pii pair<int,int>
+
+    bool countLineIntersections(vector<pii>& coordinates){
+        int lines = 0;
+        int overlap = 0;
+        for(int i=0;i<coordinates.size();++i){
+            if(coordinates[i].second==0)    overlap--;
+            else                            overlap++;
+            if(overlap==0)
+                lines++;
+        }
+        return lines>=3;
+    }
+public:
+    bool checkValidCuts(int n, vector<vector<int>>& rectangles) {
+        vector<pii> y_cordinates,x_cordinates;
+        for(auto& rectangle: rectangles){
+            y_cordinates.push_back(make_pair(rectangle[1],1));
+            y_cordinates.push_back(make_pair(rectangle[3],0));
+            x_cordinates.push_back(make_pair(rectangle[0],1));
+            x_cordinates.push_back(make_pair(rectangle[2],0));
+        }
+        sort(y_cordinates.begin(),y_cordinates.end());
+        sort(x_cordinates.begin(),x_cordinates.end());
+
+        //Line-Sweep on x and y cordinates
+        return (countLineIntersections(y_cordinates) or countLineIntersections(x_cordinates));
+    }
+};
