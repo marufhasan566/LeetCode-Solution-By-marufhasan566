c7a765bc1162e0a01a953809ce04db193423b345
marufhasan566 <marufhasan.mh1@gmail.com>
2025-04-23 10:18:34 +0600

    Added 1399

---
+++ 1399._Count_Largest_Group.cpp
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/count-largest-group/description/?envType=daily-question&envId=2025-04-23
+// https://www.youtube.com/watch?v=OKlmJNduTYU&ab_channel=codestorywithMIK
+
+class Solution {
+public:
+    int findDigitsSum(int num) {
+        int sum = 0;
+        while (num) {
+            sum += num % 10;
+            num /= 10;
+        }
+        return sum;
+    }
+    int countLargestGroup(int n) {
+        unordered_map<int, int> mp;
+        int maxSize = 0;
+        int count = 0;
+
+        for (int num = 1; num <= n; num++) {
+            int digitsSum = findDigitsSum(num);
+
+            mp[digitsSum]++;
+            if (mp[digitsSum] == maxSize) {
+                count++;
+            } else if (mp[digitsSum] > maxSize) {
+                maxSize = mp[digitsSum];
+                count = 1;
+            }
+        }
+
+        return count;
+    }
+};
