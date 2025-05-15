05d27750b5e0901f26e9af17ae204648d693c16f
marufhasan566 <marufhasan.mh1@gmail.com>
2025-05-15 08:21:14 +0600

    Added 2900

---
+++ 2900._Longest_Unequal_Adjacent_Groups_Subsequence_I.cpp
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/longest-unequal-adjacent-groups-subsequence-i/description/?envType=daily-question&envId=2025-05-15
+// https://www.youtube.com/watch?v=cbDNlWP2e_c&ab_channel=RSaiSiddhu
+
+class Solution
+{
+public:
+    vector<string> getLongestSubsequence(vector<string> &words,
+                                         vector<int> &groups)
+    {
+        vector<string> ans;
+        int n = words.size();
+        int prev;
+        for (int i = 0; i < n; i++)
+        {
+            vector<string> temp;
+            temp.push_back(words[i]);
+            prev = groups[i];
+            for (int j = i + 1; j < n; j++)
+            {
+                if (groups[j] != prev)
+                {
+                    temp.push_back(words[j]);
+                    prev = groups[j];
+                }
+            }
+            if (temp.size() > ans.size())
+                ans = temp;
+        }
+        return ans;
+    }
+};
