c2f6c5807a17052586302b0ef127cf37b64bc97e
marufhasan566 <marufhasan.mh1@gmail.com>
2025-05-19 11:30:55 +0600

    Solution of 3024

---
+++ 3024._Type_of_Triangle.cpp
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/type-of-triangle/description/?envType=daily-question&envId=2025-05-19
+
+class Solution {
+public:
+    string triangleType(vector<int>& nums) {
+        if (nums[0] + nums[1] > nums[2] && nums[1] + nums[2] > nums[0] &&
+            nums[2] + nums[0] > nums[1]) {
+            if (nums[0] == nums[1] && nums[1] == nums[2])
+                return "equilateral";
+            else if (nums[0] == nums[1] || nums[1] == nums[2] ||
+                     nums[0] == nums[2])
+                return "isosceles";
+            else
+                return "scalene";
+        } else
+            return "none";
+    }
+};
