0aa57c0f4057fe45403dbcafc0f5979373df2716
marufhasan566 <marufhasan.mh1@gmail.com>
2025-06-07 20:31:07 +0600

    Added 3170

---
+++ 3170._Lexicographically_Minimum_String_After_Removing_Stars.cpp
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/lexicographically-minimum-string-after-removing-stars/description/?envType=daily-question&envId=2025-06-07
+// https://www.youtube.com/watch?v=e528VYyaxVQ&ab_channel=Techdose
+
+class Solution
+{
+public:
+    string clearStars(string s)
+    {
+        vector<vector<int>> char_pos(26);
+        for (int i = 0; s[i]; ++i)
+        {
+            if (s[i] == '*')
+            {
+                for (int j = 0; j < 26; ++j)
+                {
+                    if (char_pos[j].size() > 0)
+                    {
+                        s[char_pos[j].back()] = '*';
+                        char_pos[j].pop_back();
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                char_pos[s[i] - 'a'].push_back(i);
+            }
+        }
+        string res;
+        for (char c : s)
+            if (c != '*')
+                res.push_back(c);
+        return res;
+    }
+};
