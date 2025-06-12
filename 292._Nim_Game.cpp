76dfc6d3a49ccf9b5218e29d64e731e3309a155a
marufhasan566 <marufhasan.mh1@gmail.com>
2025-06-12 20:43:03 +0600

    Added 291

---
+++ 292._Nim_Game.cpp
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/nim-game/description/
+class Solution
+{
+public:
+    bool canWinNim(int n)
+    {
+        if (n % 4 == 0)
+            return false;
+        return true;
+    }
+};
