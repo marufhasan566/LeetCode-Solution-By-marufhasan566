3ee44f5d4f6de70b71ef4f3794be10060c07e08a
marufhasan566 <marufhasan.mh1@gmail.com>
2025-06-20 18:36:58 +0600

    Added 2966, 2294

---
+++ 2294._Partition_Array_Such_That_Maximum_Difference_Is_K.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/partition-array-such-that-maximum-difference-is-k/description/
+// https://www.youtube.com/watch?v=mdGdUmw-W3w&ab_channel=Techdose
+
+class Solution
+{
+public:
+    int partitionArray(vector<int> &nums, int k)
+    {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+
+        int subsequences = 1;
+        int min_val = nums[0];
+        for (int i = 1; i < n; ++i)
+        {
+            if (nums[i] - min_val > k)
+            {
+                min_val = nums[i];
+                subsequences++;
+            }
+        }
+        return subsequences;
+    }
+};
---
+++ 2966._Divide_Array_Into_Arrays_With_Max_Difference.cpp
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/divide-array-into-arrays-with-max-difference/description/
+// https://www.youtube.com/watch?v=c9jSI6qjoCs&ab_channel=Techdose
+
+class Solution {
+public:
+    vector<vector<int>> divideArray(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+
+        vector<vector<int>> res;
+        for (int i = 2; i < n; i += 3) {
+            if (nums[i] - nums[i - 2] > k)
+                return {};
+            res.push_back({nums[i - 2], nums[i - 1], nums[i]});
+        }
+        return res;
+    }
+};
